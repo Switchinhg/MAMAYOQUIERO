b3b4f4c4766418fc42b5cba6c3f8823d85eb26f3
david r <davidresca9@gmail.com>
2022-09-22 18:03:38 -0300

    fix js

--- js/navbar.js
+++ js/navbar.js
@@ -9,10 +9,17 @@ const mainIcons = document.querySelector('.header-btn-container')
 
 const showMenu = (e) => {
     collapsibleMenu.classList.toggle('nav-open');
-    mainTitle.classList.toggle('header-title-dark');
-    mainIcons.classList.toggle('header-btn-container-open');
+    /* mainTitle.classList.toggle('header-title-dark');
+    mainIcons.classList.toggle('header-btn-container-open'); */
 }
 
 btnBurger.addEventListener('click', showMenu);
 
 btnMenuClose.addEventListener('click', showMenu);
+
+
+window.addEventListener('click', (e)=>{
+    if (!collapsibleMenu.contains(e.target) && (!btnMenuClose.contains(e.target)) && (!btnBurger.contains(e.target))){
+        collapsibleMenu.classList.remove('nav-open');
+    } 
+})
